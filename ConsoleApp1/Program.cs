726566ff338750d57c83c2dc212247e23031bc82
Ulyana Shornikova <ushornikova@brainstorminc.com>
2019-03-27 22:30:26 +0200

    added comment

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class countNumbersinArr
+    {
+        static void Main(string[] Arg)
+        {
+           int[] someNumbers = new int[4];
+            for (int j = 0; j < 4; j++)
+            {
+                Console.WriteLine("Please enter item" + j);
+                var k = Console.ReadLine();
+                someNumbers[j] = Convert.ToInt32(k);
+            }
+            int Max = someNumbers[0];
+            for (int i=1; i<4; i++)
+            {
+                if (someNumbers[i] > Max)
+                {
+                    Max = someNumbers[i];
+                }
+            }
+            Console.WriteLine("max number is" + Max);
+            Console.ReadLine();
+        }
+    }
+}
+    
---
+++ ConsoleApp1/TextLenght.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class TextLenght
+    {
+        private void Main(string[] args)
+        {
+            string a;
+            Console.WriteLine("Please enter a text");
+            a = Console.ReadLine();
+            int strlength = a.Length;
+            Console.WriteLine("The text length is", strlength);
+            Console.ReadKey();
+            //some comment
+        }
+    }
+}
